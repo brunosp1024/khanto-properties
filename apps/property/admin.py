1dc3e72176ebe97d9aa25d58ee6dd0934bd54188
Bruno <brunosalvador145@gmail.com>
2023-09-17 10:13:44 -0300

    feat: register property model to django admin

--- apps/property/admin.py
+++ apps/property/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Property
 
-# Register your models here.
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('property_id', 'property_code', 'activation_date')
+    list_display_links = ('property_id', 'property_code')
+    search_fields = ('property_code',)
+    list_per_page = 20
+
+
+admin.site.register(Property, PropertyAdmin)
