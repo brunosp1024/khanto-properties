7f3ab61a59cdc0d9f5bec64456a94b1c927d8e5a
Bruno <brunosalvador145@gmail.com>
2023-09-18 23:07:48 -0300

    refactor: comment code block documentation

--- setup/urls.py
+++ setup/urls.py
@@ -6,6 +6,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 
+# swagger documentation
 schema_view = get_schema_view(
    openapi.Info(
       title="Khanto properties API",
