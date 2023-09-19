db75db3d7b1d2e1c2b06498ae3fa783aba9a2997
Bruno <brunosalvador145@gmail.com>
2023-09-18 23:09:17 -0300

    feat: create views file to property

--- apps/property/views.py
+++ apps/property/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Property
+from .serializers import PropertySerializer
+
+
+class PropertyViewSet(ModelViewSet):
+    """ Implements all actions to properties: list, create, retrieve, update e delete """
+
+    serializer_class = PropertySerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Property.objects.all()
