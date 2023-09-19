cad88f76bef6d58e14a2a2e4ec5c31d28b5224f6
Bruno <brunosalvador145@gmail.com>
2023-09-19 10:43:11 -0300

    feat: create viewSet to advertisement

--- apps/advertisement/views.py
+++ apps/advertisement/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ Implements actions to advertisement: list, create, retrieve e update """
+
+    serializer_class = AdvertisementSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Advertisement.objects.all()
+    http_method_names = ['get', 'post', 'put', 'patch']
