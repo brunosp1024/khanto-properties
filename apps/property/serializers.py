a96a6b1f750087f29526f87cccd88266326909c4
Bruno <brunosalvador145@gmail.com>
2023-09-19 10:45:54 -0300

    refactor: update metadata in PropertySerializer

--- apps/property/serializers.py
+++ apps/property/serializers.py
@@ -5,4 +5,8 @@ from .models import Property
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        exclude = ['deleted_at']
+        fields = [
+            'property_id', 'property_code', 'guest_limit',
+            'bathroom_count', 'pets_allowed', 'cleaning_fee',
+            'activation_date', 'created_at', 'updated_at'
+        ]
