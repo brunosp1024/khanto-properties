ab367e1b1450bae8eb6e422c2935c3f3e77c9c4a
Bruno <brunosalvador145@gmail.com>
2023-09-19 10:38:27 -0300

    feat: create advertisement model and insert in admin

--- apps/advertisement/admin.py
+++ apps/advertisement/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('advertisement_id', 'property', 'platform_name', 'platform_fee')
+    list_display_links = ('advertisement_id', 'property', 'platform_name')
+    search_fields = ('property__property_code','platform_name')
+    raw_id_fields = ('property',)
+    list_per_page = 20
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- apps/advertisement/models.py
+++ apps/advertisement/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from apps.property.models import Property
+from apps.core.models import TimestampableMixin
+from apps.core.models import DeletedMixin
+
+
+class Advertisement(TimestampableMixin, DeletedMixin):
+    advertisement_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE, null=False)
+    platform_name = models.CharField(max_length=100)
+    platform_fee = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+
+    class Meta:
+        db_table = 'advertisement'
+        ordering = ['-created_at']
+        verbose_name = 'advertisement'
+        verbose_name_plural = 'advertisements'
+
+    def __str__(self):
+        return f'{self.property}, {self.platform_name}'
+
