2c3ce16bc92a62d775cd06cc65711879ee73c94e
Bruno <brunosalvador145@gmail.com>
2023-09-20 16:21:38 -0300

    feat: create reservation model and resources

--- apps/reservation/admin.py
+++ apps/reservation/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Reservation
 
-# Register your models here.
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = (
+        'reservation_id', 'advertisement', 'reservation_code',
+        'check_in_date', 'check_out_date'
+    )
+    list_display_links = ('reservation_id', 'advertisement', 'reservation_code')
+    search_fields = ('reservation_code',)
+    raw_id_fields = ('advertisement',)
+    list_per_page = 20
+
+
+admin.site.register(Reservation, ReservationAdmin)
--- apps/reservation/models.py
+++ apps/reservation/models.py
@@ -1,3 +1,30 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from apps.advertisement.models import Advertisement
+from apps.core.models import DeletedMixin
+from apps.core.models import TimestampableMixin
+from apps.core.utils.functions_utils import generate_random_code
+
+
+class Reservation(TimestampableMixin, DeletedMixin):
+    reservation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    reservation_code = models.CharField(
+        max_length=100, default=generate_random_code, unique=True, editable=False
+    )
+    check_in_date = models.DateTimeField()
+    check_out_date = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    comment = models.TextField(null=True, blank=True)
+    num_guests = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'reservation'
+        ordering = ['reservation_code']
+        verbose_name = 'reservation'
+        verbose_name_plural = 'reservations'
+
+    def __str__(self):
+        return f'{self.reservation_code}, {self.advertisement}'
