4b0038b171acc693487902454ce5a335cb208d4f
Bruno <brunosalvador145@gmail.com>
2023-09-22 16:59:29 -0300

    refactor: add validation to util function

--- apps/core/utils/functions_utils.py
+++ apps/core/utils/functions_utils.py
@@ -5,5 +5,11 @@ import string
 def generate_random_code(length=8):
     """ Generate and return string with 8 characteres """
 
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(length))
+    try:
+        if length < 1:
+            raise ValueError("Length must be a positive integer")
+
+        characters = string.ascii_letters + string.digits
+        return ''.join(random.choice(characters) for _ in range(length))
+    except Exception:
+        raise Exception('Error at generate code')
