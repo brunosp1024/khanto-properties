1bb67639284f46175ddbb7ae54a2e80ddab5b0fc
Bruno <brunosalvador145@gmail.com>
2023-09-23 14:17:12 -0300

    refactor: adds validation to create new reservation

--- apps/reservation/views.py
+++ apps/reservation/views.py
@@ -5,6 +5,7 @@ from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from apps.advertisement.models import Advertisement
 
 from .models import Reservation
 from .serializers import ReservationSerializer
@@ -21,8 +22,21 @@ class ReservationViewSet(ModelViewSet):
 
     def create(self, request):
         try:
+            advertisement = Advertisement.objects.get(pk=request.data['advertisement'])
             check_in = parser.parse(request.data['check_in_date'])
             check_out = parser.parse(request.data['check_out_date'])
+
+            has_conflicting_reservations = Reservation.objects.filter(
+                advertisement__property=advertisement.property,
+                check_out_date__gt=check_in
+            ).exists()
+
+            if has_conflicting_reservations:
+                return Response(
+                    {'message': 'There is already a reservation for this property.'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
             if check_in > check_out:
                 return Response(
                     {'message': 'Check-in cannot be later than check-out'},
